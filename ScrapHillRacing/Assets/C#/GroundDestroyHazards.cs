cb1137a909f2157c316b35d58a1cd157cb20a5c9
oppilas <oppilas@htkmtav-41814.dyn.edu.hel.fi>
2020-02-01 21:14:39 +0200

    Hazards and movement tweaking

---
+++ ScrapHillRacing/Assets/C#/GroundDestroyHazards.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundDestroyHazards : MonoBehaviour
+{
+    public GameObject explosion;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "flyinghazard")
+        {
+            Instantiate(explosion, other.transform.position, Quaternion.identity, null);
+
+            Destroy(other.gameObject);
+        }
+    }
+}
