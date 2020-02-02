debd029822d801e0c0467533c12611ff5876793c
oppilas <oppilas@htkmtav-41814.dyn.edu.hel.fi>
2020-02-02 15:19:59 +0200

    sound juice and stuff

---
+++ ScrapHillRacing/Assets/ScrapSound/Scripts/EnginePitch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnginePitch : MonoBehaviour
+{
+    public float topSpeed = 100; // km per hour
+    private float currentSpeed = 0;
+    private float pitch = 0;
+
+    void Update()
+    {
+        currentSpeed = transform.GetComponent<Rigidbody>
+        ().velocity.magnitude * 3.6f;
+        pitch = currentSpeed / topSpeed + 0.5f;
+
+        pitch = Mathf.Clamp(pitch, 0.5f, 2f);
+
+        transform.GetComponent<AudioSource>().pitch =
+        pitch;
+
+        if (pitch < 1.5) ;
+
+    }
+}
---
+++ ScrapHillRacing/Assets/ScrapSound/Scripts/SoundRandomizer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundRandomizer : MonoBehaviour
+{
+    public AudioClip[] clipArray;
+    public AudioSource effectSource;
+    public float pitchMin, pitchMax, volumeMin, volumeMax;
+    private int clipIndex;
+    // Start is called before the first frame update
+
+    void PlayRoundRobin()
+    {
+        effectSource.pitch = Random.Range(pitchMin, pitchMax);
+        effectSource.volume = Random.Range(volumeMin, volumeMax);
+        if (clipIndex < clipArray.Length)
+        {
+            effectSource.PlayOneShot(clipArray[clipIndex]);
+            clipIndex++;
+        }
+
+        else
+        {
+            clipIndex = 0;
+            effectSource.PlayOneShot(clipArray[clipIndex]);
+            clipIndex++;
+        }
+    }
+    void PlayRandom()
+    {
+        effectSource.pitch = Random.Range(pitchMin, pitchMax);
+        effectSource.volume = Random.Range(volumeMin, volumeMax);
+        clipIndex = Random.Range(0, clipArray.Length);
+        effectSource.PlayOneShot(clipArray[clipIndex]);
+    }
+    int RepeatCheck(int previousIndex, int range)
+    {
+        int index = Random.Range(0, range);
+
+        while (index == previousIndex)
+        {
+            index = Random.Range(0, range);
+        }
+        return index;
+    }
+    void PlayRandom2()
+    {
+        clipIndex = RepeatCheck(clipIndex, clipArray.Length);
+        effectSource.PlayOneShot(clipArray[clipIndex]);
+    }
+    void Update()
+    {
+        if(!effectSource.isPlaying)
+            {
+                PlayRoundRobin();
+        }
+    }
+}
